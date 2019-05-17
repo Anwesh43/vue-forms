9458912fb272f1ca3259588e14e908bfc1555776
Anwesh43 <anweshthecool0@gmail.com>
2019-05-17 23:35:34 +0530

    using computed properties

--- index.js
+++ index.js
@@ -29,5 +29,25 @@ const vueInstance = new Vue({
         resetFormValidityText() {
             this.formValidityText = ''
         }
+    },
+
+    computed : {
+        isInvalidAge() {
+            return this.form.age > 120 || this.form.age < 18
+        },
+
+        isNameTooLong() {
+            return this.form.name.length > 100
+        },
+
+        isFormValidCompute() {
+            const {name, age} = this.form
+            console.log(this.isNameTooLong)
+            var isNameValid = !!name && !this.isNameTooLong
+            console.log(typeof(age))
+            var isAgeValid = typeof(age) === "number" && age >= 18 && age <= 120
+            console.log(`nameValid, ageValid: ${isNameValid}, ${isAgeValid}`)
+            return isNameValid && isAgeValid
+        }
     }
 })
--- template.html.js
+++ template.html.js
@@ -5,16 +5,16 @@ module.exports = (url) => {
                 <div>
                     <span>Name</span>
                     <input type="text" v-model = "form.name" @keydown = "resetFormValidityText">
-
+                    <span v-if="isNameTooLong" :style = "{color : 'red'}">Name is too long</span>
                     <span v-if="!isNameValid">Please enter a name</span>
                 </div>
                 <div>
                 <span>Age</span>
                 <input type="text" v-model.number = "form.age" @keydown = "resetFormValidityText">
-                <span v-if="!isAgeValid">Please enter an age from 18 to 120</span>
+                <span v-if="isInvalidAge">Please enter an age from 18 to 120</span>
                 </div>
                 <div>{{formValidityText}}</div>
-                <button>submit</button>
+                <button :disabled = "!isFormValidCompute">submit</button>
             </form>
         </div>
         <script src="${url}">
